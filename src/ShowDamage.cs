397873c79c1d9ad889ab7a295ee288a1e93488f3
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2025-03-07 13:31:23 +0500

    Update 1.DZ.3
    -Added CFG file

--- src/ShowDamage.cs
+++ src/ShowDamage.cs
@@ -3,22 +3,68 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Capabilities;
 using CounterStrikeSharp.API.Modules.Utils;
 using CS2_GameHUDAPI;
+using System.Text.Json.Serialization;
 
 namespace CS2_ShowDamage
 {
-    public class ShowDamage : BasePlugin
+	public class HUDConfig : BasePluginConfig
 	{
-		static readonly byte HUDCHANNEL_DAMAGE = 15;
-		static readonly byte HUDCHANNEL_SELFDAMAGE = 16;
-
+		[JsonPropertyName("Channel_Damage")] public byte HUDCHANNEL_DAMAGE { get; set; } = 15;
+		[JsonPropertyName("Channel_SelfDamage")] public byte HUDCHANNEL_SELFDAMAGE { get; set; } = 16;
+		[JsonPropertyName("Size")] public int HUDSIZE { get; set; } = 32;
+		[JsonPropertyName("Font")] public string HUDFONT { get; set; } = "Verdana";
+		[JsonPropertyName("WorldUnitsPerPx")] public float HUDUNITS { get; set; } = 0.007f;
+		[JsonPropertyName("Time")] public float HUDTIME_DAMAGE { get; set; } = 2.0f;
+		[JsonPropertyName("Time_Self")] public float HUDTIME_SELFDAMAGE { get; set; } = 2.0f;
+		[JsonPropertyName("Radius")] public float HUDRADIUS { get; set; } = 0.7f;
+		[JsonPropertyName("Self_PosX")] public float HUDSELFX { get; set; } = 0.0f;
+		[JsonPropertyName("Self_PosY")] public float HUDSELFY { get; set; } = -0.7f;
+	}
+	public class ShowDamage : BasePlugin, IPluginConfig<HUDConfig>
+	{
+		public HUDConfig Config { get; set; } = new HUDConfig();
 		static readonly Random rd = new();
-		static readonly Vector g_vecSelfDamage = new(0, -0.7f, 7);
+		static Vector g_vecSelfDamage = new(0, -0.7f, 7);
 		static Vector[] g_vecPlayer = new Vector[65];
 		static IGameHUDAPI? _api;
 		public override string ModuleName => "Show Damage";
 		public override string ModuleDescription => "Shows the damage dealt to the player";
 		public override string ModuleAuthor => "DarkerZ [RUS]";
-		public override string ModuleVersion => "1.DZ.2";
+		public override string ModuleVersion => "1.DZ.3";
+		public void OnConfigParsed(HUDConfig config)
+		{
+			if (config.HUDCHANNEL_DAMAGE < 0 || config.HUDCHANNEL_DAMAGE > 32)
+				config.HUDCHANNEL_DAMAGE = 15;
+
+			if (config.HUDCHANNEL_SELFDAMAGE < 0 || config.HUDCHANNEL_SELFDAMAGE > 32)
+				config.HUDCHANNEL_SELFDAMAGE = 15;
+
+			if (config.HUDSIZE < 16 || config.HUDSIZE > 256)
+				config.HUDSIZE = 32;
+
+			if (string.IsNullOrEmpty(config.HUDFONT))
+				config.HUDFONT = "Verdana";
+
+			if (config.HUDTIME_DAMAGE < 0.1f || config.HUDTIME_DAMAGE > 10.0f)
+				config.HUDTIME_DAMAGE = 2.0f;
+
+			if (config.HUDTIME_SELFDAMAGE < 0.1f || config.HUDTIME_SELFDAMAGE > 10.0f)
+				config.HUDTIME_SELFDAMAGE = 2.0f;
+
+			if (config.HUDRADIUS < 0.1f || config.HUDRADIUS > 10.0f)
+				config.HUDRADIUS = 0.7f;
+
+			if (config.HUDSELFX < -10.0f || config.HUDSELFX > 10.0f)
+				config.HUDSELFX = 0.0f;
+
+			if (config.HUDSELFY < -10.0f || config.HUDSELFY > 10.0f)
+				config.HUDSELFY = -0.7f;
+
+			Config = config;
+
+			g_vecSelfDamage.X = Config.HUDSELFX;
+			g_vecSelfDamage.Y = Config.HUDSELFY;
+		}
 		public override void OnAllPluginsLoaded(bool hotReload)
 		{
 			try
@@ -55,16 +101,16 @@ namespace CS2_ShowDamage
 			int iDamage = @event.DmgHealth;
 			if (player != null && player.IsValid)
 			{
-				_api.Native_GameHUD_UpdateParams(player, HUDCHANNEL_SELFDAMAGE, g_vecSelfDamage, System.Drawing.Color.Red, 32, "Verdana", 0.007f);
-				_api.Native_GameHUD_Show(player, HUDCHANNEL_SELFDAMAGE, $"-{iDamage}", 2.0f);
+				_api.Native_GameHUD_UpdateParams(player, Config.HUDCHANNEL_SELFDAMAGE, g_vecSelfDamage, System.Drawing.Color.Red, Config.HUDSIZE, Config.HUDFONT, Config.HUDUNITS);
+				_api.Native_GameHUD_Show(player, Config.HUDCHANNEL_SELFDAMAGE, $"-{iDamage}", Config.HUDTIME_SELFDAMAGE);
 			}
 			if (attacker != null && attacker.IsValid)
 			{
 				double r = rd.NextDouble() * 2 * Math.PI;
-				g_vecPlayer[attacker.Slot].X = (float)Math.Cos(r) * 0.7f - 0.1f;
-				g_vecPlayer[attacker.Slot].Y = (float)Math.Sin(r) * 0.7f + 0.1f;
-				_api.Native_GameHUD_UpdateParams(attacker, HUDCHANNEL_DAMAGE, g_vecPlayer[attacker.Slot], System.Drawing.Color.Aqua, 32, "Verdana", 0.007f);
-				_api.Native_GameHUD_Show(attacker, HUDCHANNEL_DAMAGE, $"{iDamage}", 2.0f);
+				g_vecPlayer[attacker.Slot].X = (float)Math.Cos(r) * Config.HUDRADIUS - 0.1f;
+				g_vecPlayer[attacker.Slot].Y = (float)Math.Sin(r) * Config.HUDRADIUS + 0.1f;
+				_api.Native_GameHUD_UpdateParams(attacker, Config.HUDCHANNEL_DAMAGE, g_vecPlayer[attacker.Slot], System.Drawing.Color.Aqua, Config.HUDSIZE, Config.HUDFONT, Config.HUDUNITS);
+				_api.Native_GameHUD_Show(attacker, Config.HUDCHANNEL_DAMAGE, $"{iDamage}", Config.HUDTIME_DAMAGE);
 			}
 			return HookResult.Continue;
 		}
@@ -86,8 +132,8 @@ namespace CS2_ShowDamage
 		{
 			if (_api != null && player != null && player.IsValid)
 			{
-				_api.Native_GameHUD_SetParams(player, HUDCHANNEL_DAMAGE, g_vecPlayer[player.Slot], System.Drawing.Color.Aqua, 32, "Verdana", 0.007f, PointWorldTextJustifyHorizontal_t.POINT_WORLD_TEXT_JUSTIFY_HORIZONTAL_CENTER);
-				_api.Native_GameHUD_SetParams(player, HUDCHANNEL_SELFDAMAGE, g_vecSelfDamage, System.Drawing.Color.Red, 32, "Verdana", 0.007f, PointWorldTextJustifyHorizontal_t.POINT_WORLD_TEXT_JUSTIFY_HORIZONTAL_CENTER);
+				_api.Native_GameHUD_SetParams(player, Config.HUDCHANNEL_DAMAGE, g_vecPlayer[player.Slot], System.Drawing.Color.Aqua, Config.HUDSIZE, Config.HUDFONT, Config.HUDUNITS, PointWorldTextJustifyHorizontal_t.POINT_WORLD_TEXT_JUSTIFY_HORIZONTAL_CENTER);
+				_api.Native_GameHUD_SetParams(player, Config.HUDCHANNEL_SELFDAMAGE, g_vecSelfDamage, System.Drawing.Color.Red, Config.HUDSIZE, Config.HUDFONT, Config.HUDUNITS, PointWorldTextJustifyHorizontal_t.POINT_WORLD_TEXT_JUSTIFY_HORIZONTAL_CENTER);
 			}
 		}
 
@@ -95,8 +141,8 @@ namespace CS2_ShowDamage
 		{
 			if (_api != null && player != null && player.IsValid)
 			{
-				_api.Native_GameHUD_Remove(player, HUDCHANNEL_DAMAGE);
-				_api.Native_GameHUD_Remove(player, HUDCHANNEL_SELFDAMAGE);
+				_api.Native_GameHUD_Remove(player, Config.HUDCHANNEL_DAMAGE);
+				_api.Native_GameHUD_Remove(player, Config.HUDCHANNEL_SELFDAMAGE);
 			}
 		}
 	}
